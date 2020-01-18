b00f5ad00c9a31ba4ee44662029196b1de1544e7
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 06:14:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,11 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  i = 0 
+  arr = []
+  while i < source.length do 
+    
+  
 end
 
 def total_gross(source)
@@ -38,6 +43,9 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total = 0 
+  directorName = list_of_directors
+  directorTotals = directors_totals(source)
 end
 
 
